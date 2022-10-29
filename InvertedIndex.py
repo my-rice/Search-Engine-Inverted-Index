688d4c440754bf5eef2abef7a51e11e6fbdcd383
my-rice <dvdrsi2017@gmail.com>
2022-10-29 13:01:02 +0200

    ...

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -1,12 +1,26 @@
 def InvertedIndex:
+    """ Questa classe implementa un dizionario che memorizza coppie di chiave-valore (w,L). 
+        Le chiavi del dizionario sono chiamate index terms. Sono un set di entry di un vocabulary e nomi propri lunghi il più possibile.
+        I valori del dizionario sono chiamati occurence lists. Essi contengono quante pagine Web possono contenere.
+        In realtà le vere occurence list sono oggetti separati. In questa classe verranno memeorizzati SOLO i riferimenti a questi oggetti occurence list.
+        Nota: L'occurrence list memorizza anche il numero di occorrenze della parola nella pagina.
+    """
     def __init__(self):
+        """Crea un nuovo oggetto InvertedIndex"""
         pass
 
     def addWord(self, keyword):
+        """Aggiunge la stringa keyworld nell'indice"""
         pass
 
     def addPage(self, page):
+        """Processa la webpage passata per parametro. Per ogni parola della webpage, essa viene inserita nell'inverted index se non è presente e la pagina viene inserita nell'occurrence list.
+           Inoltre, viene aggiornato numero di occorrenze della parola nella pagina nell'occurrence list.
+        """
         pass
 
     def getList(self, keyword):
+        """Prende in input la stringa keyword e restituisce la corrispondente occurence list. 
+           Lancia un'eccezione se non c'è una occurence list associata alla stringa keyword
+        """
         pass
--- engine.py
+++ engine.py
@@ -1,7 +1,16 @@
 def SearchEngine:
 
     def __init__(self, namedir):
+        """Inizializza il motore di ricerca. Prende in input una directory (namedir) nel quale ci sono molti file ognuno dei quali rappresenta una differente webpage. 
+            Ogni file contiene: nella prima riga l'URL (incluso l'hostname); in tutte le altre righe il contenuto della webpage. 
+            Questa funzione popola il database del motore di ricerca, inizializzando e inserendo i valori nelle strutture dati necessarie.
+        """
+        """Nel nostro caso prende in input la cartella "dataset" e popola tutte le necessarie strutture dati."""
         pass
 
     def search(self, keyword, k):
+        """Cerca le k (un intero) pagine web con il massimo numero di occorrenze della keyword cercata. 
+           Restituisce una stringa s costruita come segue: 
+           per ognuna di queste k pagine, sono ordinate in ordine decrescente di occorrenze, la stringa del website del sito che hosta la pagina è aggiunta a s, a meno che il website non sia stato già inserito.  
+        """
         pass
