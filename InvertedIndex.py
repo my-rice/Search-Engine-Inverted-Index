5ff9f1f4c9a0c40891719cb42ffb9f3178f35db8
my-rice <dvdrsi2017@gmail.com>
2022-10-29 12:06:37 +0200

    ...

---
+++ InvertedIndex.py
@@ -0,0 +1,12 @@
+def InvertedIndex:
+    def __init__(self):
+        pass
+
+    def addWord(self, keyword):
+        pass
+
+    def addPage(self, page):
+        pass
+
+    def getList(self, keyword):
+        pass
