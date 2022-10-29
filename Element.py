2163e2fcdd450a5e3bff1f632430c160b2c7c904
my-rice <dvdrsi2017@gmail.com>
2022-10-29 11:02:42 +0200

    ..

---
+++ Element.py
@@ -0,0 +1,4 @@
+def Element:
+    """"""
+    def __init__():
+        pass
---
+++ WebSite.py
@@ -0,0 +1,46 @@
+import Element
+def WebSite:
+
+    _homepage
+
+    def __init__(self, host):
+        """È il costruttore della classe WebSite. Crea un nuovo oggetto WebSite per salvare il WebSite dell'host."""
+        _homepage = Element()
+        pass
+
+    def __isDir(self, elem):
+        pass
+
+    def __isPage(self, elem):
+
+    def __hasDir(self, ndir, cdir):
+        pass
+
+    def __newDir(self, ndir, cdir):
+        pass
+
+    def __hasPage(self, npag, cdir):
+        pass
+
+    def __newPage(self, npag, cdir):
+        pass
+
+    def getHomePage(self):
+        """Restituisce l'home page del website al quale l'oggetto corrente si riferisce. Oppure lancia un eccezione se l'homepage non esiste"""
+        pass
+
+    def getSiteString(self):
+        """Restituisce una stringa che mostra la struttura del website. (La stringa è formattata in un certo modo)."""
+
+        pass
+
+    def insertPage(self, url, content):
+        """ Salva e restituisce la nuova pagina del website. 
+        URL: è una stringa che rappresenta l'URL della pagina.
+        content: è una stringa che rappresenta il testo contenuto nella pagina.
+        """
+        pass
+
+    def getSiteFromPage(self, page):
+        """Data una webpage, restituisce l'oggetto website al quale la pagina appartiene."""
+        pass
